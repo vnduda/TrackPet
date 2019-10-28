f7209dbcadde60a491cf5f8932cab6c5203753ed
Joao Victor <joaobrvictor@gmail.com>
2019-10-28 15:53:49 -0300

    progesso de login

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React, { Fragment } from 'react';
-import { View, Text } from 'react-native';
-// import LoginController from './LoginController';
+import LoginController from './LoginController';
 
 const App = () => {
   return (
-    <View>
-      <Text>Test</Text>
-    </View>
+    <LoginController />)
   );
 };
 

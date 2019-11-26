39afb47649909ad84c65ca22999534f657bdcd8b
Ana Luísa Burjack <analuisaburjack@gmail.com>
2019-11-26 15:28:10 -0300

    Adição da coluna NOme no banco

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_145228) do
+ActiveRecord::Schema.define(version: 2019_11_26_182522) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_11_24_145228) do
     t.text "ps"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|

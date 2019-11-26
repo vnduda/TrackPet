6f7dcb39a9e620333bea2b90715a66c8e864011f
Ana Luísa Burjack <analuisaburjack@gmail.com>
2019-11-26 06:50:28 -0300

    Criação das Páginas de layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 //= require jquery
+//=require jquery_ujs
 //= require bootstrap-sprockets
+//=require turbolinks
+//= require_tree
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome_page/index'
+  root to: 'welcome_page#index'
   resources :animal_losts
   resources :animal_founds
   devise_for :users
-  root 'welcome_page#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

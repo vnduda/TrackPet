3cf59c53d3420cf678f1266deb177189676df4d5
Duda <eduardavieiranetto@gmail.com>
2019-12-03 19:27:54 -0300

    Testes organizados

--- app/models/animal_found.rb
+++ app/models/animal_found.rb
@@ -4,4 +4,10 @@
 # um modelo e uma imagem.
 class AnimalFound < ApplicationRecord
   has_one_attached :image
+  validates_presence_of :image
+  validates_presence_of :foundplace
+  validates_presence_of :founddate
+  validates_presence_of :specie
+  validates_presence_of :contact
+
 end
--- app/models/animal_lost.rb
+++ app/models/animal_lost.rb
@@ -4,4 +4,9 @@
 # um modelo e uma imagem.
 class AnimalLost < ApplicationRecord
   has_one_attached :image
+  validates_presence_of :image
+  validates_presence_of :lostplace
+  validates_presence_of :lostdate
+  validates_presence_of :specie
+  validates_presence_of :contact
 end
